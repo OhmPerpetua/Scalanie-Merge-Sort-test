ba8c2d4616dcdc52f2203bb48e0cb7e9d05bedbc
Filip <filip.wachala@op.pl>
2024-12-05 09:39:32 +0100

    main

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "MergeSort.h"
+
+int main() {
+    std::vector<int> array = { 38, 27, 43, 3, 9, 82, 10 };
+    MergeSort sorter;
+    sorter.sort(array);
+    for (int num : array) {
+        std::cout << num << " ";
+    }
+    std::cout << std::endl;
+    return 0;
+}
